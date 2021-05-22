8516f3ff3b4da231a693e27eded29577f849c2a6
tiko manukyan <tikomanukyan44@gmail.com>
2021-05-22 20:42:40 +0400

    header

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './header/search/search.component';
+import { AccountLinksComponent } from './header/account-links/account-links.component';
+import { NavComponent } from './header/nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    AccountLinksComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
